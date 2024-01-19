f2c70c12453feb1ca865c75b3b5cb1cd51b134d0
Rutuja Rathod <127573564+18Rutuja@users.noreply.github.com>
2024-01-19 17:57:27 +0000

    .

---
+++ middleofLL.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    Node* next;
+    int val;
+    Node(int data){
+    val = data;
+    next = NULL;
+    }
+};
+class LinkedList{
+        Node* head;
+};
+ 
